df0c1f6fcb5e7740451e50e1458f680827d04761
Abigail <abigailf03@hotmail.ca>
2025-03-06 10:49:46 -0600

    basic timer functionality working

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,23 +1,51 @@
-var myTimer = null;
-var time = 0;
+var myTimer = null; //for setInterval
+var time = 1500; //time of timer
+var isRunning = false; //is timer started or stopped
 
-function updateTimer() {
-  time++;
-  document.getElementById("timer").innerHTML = time; //displays time
+function generateTime() {
+  var second = time % 60;
+  var minute = Math.floor(time / 60) % 60;
+
+  document.querySelector(".second").textContent =
+    second < 10 ? "0" + second : second;
+  document.querySelector(".minute").textContent =
+    minute < 10 ? "0" + minute : minute;
 }
 
-function startTimer() {
-  if (!myTimer) {
-    // Prevent multiple intervals
-    myTimer = setInterval(updateTimer, 1000);
+//updates time
+function updateTimer() {
+  if (time > 0) {
+    time--;
+    generateTime();
+  } else {
+    stopTimer();
   }
 }
 
-function stopTimer() {
-  clearInterval(myTimer); //stops timer
-  myTimer = null;
+//starts timer
+function startTimer(interval = 1000) {
+  if (!isRunning) {
+    isRunning = true; // Timer is running
+    // Prevent multiple intervals
+    myTimer = setInterval(updateTimer, interval);
+  }
 }
 
+//stops timer
+function stopTimer() {
+  clearInterval(myTimer);
+  isRunning = false;
+  alert("Time is up!");
+}
+
+//resets timer - not sure if will use
+function resetTimer() {
+  stopTimer();
+  time = 1500; //25 minutes
+  generateTime();
+}
+
+//load on page
 window.onload = function () {
-  document.getElementById("timer").innerHTML = time; // Ensures initial display
+  generateTime();
 };
